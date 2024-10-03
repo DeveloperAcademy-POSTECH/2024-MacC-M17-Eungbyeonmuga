7cc6092dc9fc70ba9474dd737044454d9a0ec040
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-10-03 22:39:05 +0900

    add project

---
+++ RookieKBO/RookieKBO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RookieKBO
+//
+//  Created by crownjoe on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RookieKBO/RookieKBO/RookieKBOApp.swift
@@ -0,0 +1,17 @@
+//
+//  RookieKBOApp.swift
+//  RookieKBO
+//
+//  Created by crownjoe on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct RookieKBOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
