2a919b742775f36306bbeca4d44a5abe46cd5fbd
KyungsooLee <jmtkd9196@gmail.com>
2024-10-07 12:29:52 +0900

    [#1] State 열거형 엔티티 구현

---
+++ RookieKBO/RookieKBO/Domain/Entity/State.swift
@@ -0,0 +1,24 @@
+//
+//  State.swift
+//  RookieKBO
+//
+//  Created by kyungsoolee on 10/7/24.
+//
+
+import Foundation
+
+enum State: Codable, CaseIterable {
+    case PREPARE
+    case PLAYING
+    case END
+    case CANCEL
+    
+    var korean: String {
+        switch self {
+        case .PREPARE: return "준비"
+        case .PLAYING: return "경기중"
+        case .END: return "종료"
+        case .CANCEL: return "취소"
+        }
+    }
+}
