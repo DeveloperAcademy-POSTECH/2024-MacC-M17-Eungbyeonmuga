c0cb0c1db085c8b7896c25da52184ea5fb705532
omj0722 <omj0722@icloud.com>
2024-10-08 10:14:46 +0900

    [#3] 응원 팀 선택 버튼 구현(임시)

--- RookieKBO/RookieKBO/Presentation/SelectTeamView.swift
+++ RookieKBO/RookieKBO/Presentation/SelectTeamView.swift
@@ -39,17 +39,50 @@ private struct HeaderView: View {
 private struct SelectTeamListView: View {
     
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    let teams: [Team] = [
+        Team(name: "SSG 랜더스", image: "팀1"),
+        Team(name: "KIA 타이거즈", image: "팀2"),
+        Team(name: "KT 위즈", image: "팀3"),
+        Team(name: "LG 트윈스", image: "팀4"),
+        Team(name: "NC 다이노스", image: "팀5"),
+        Team(name: "두산 베어스", image: "팀6"),
+        Team(name: "롯데 자이언츠", image: "팀7"),
+        Team(name: "삼성 라이온즈", image: "팀8"),
+        Team(name: "키움 히어로즈", image: "팀9"),
+        Team(name: "한화 이글즈", image: "팀10"),
+        Team(name: "전체 구단", image: "팀11")
+    ]
     
     var body: some View {
         ScrollView{
             LazyVGrid(columns: columns) {
-                ForEach(0..<11) { index in // TODO: 0..<11 대신 팀으로 변경
+                ForEach(teams) { team in
                     Button {
-                        print("\(index) 번째 팀")
+                        print("\(team) 번째 팀")
                     } label: {
-                        RoundedRectangle(cornerRadius: 16) // TODO: 팀 이미지, 팀 명으로 변경
-                            .frame(width: .infinity, height: 172)
-                            .foregroundColor(.gray)
+                        ZStack{
+                            VStack {
+                                HStack{
+                                    Text(team.name)
+                                    Spacer()
+                                }
+                                .padding(.init(top: 8, leading: 32, bottom: 0, trailing: 0))
+                                Spacer()
+                            }
+                            
+                            Image(systemName:"pencil.circle.fill") // TODO: team.image로 변경
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 150, height: 150)
+                                .padding(.init(top: 30, leading: 50, bottom: 0, trailing: 0))
+                        }
+                        .frame(width: 172, height: 172)
+                        .clipped()
+                        .background(RoundedRectangle(cornerRadius: 18)
+                            .fill(Color.white)
+                            .stroke(.gray, lineWidth: 5))
+                        
+                        
                     }
                     .padding(8)
                 }
