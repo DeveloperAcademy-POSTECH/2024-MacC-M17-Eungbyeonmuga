b49d7a31780e06bfc121f34ce5df32dec208aced
KyungsooLee <jmtkd9196@gmail.com>
2024-10-18 23:47:56 +0900

    [#21] SeasonTypeLabel 구현

---
+++ RookieKBO/RookieKBO/Component/SeasonTypeLabel.swift
@@ -0,0 +1,29 @@
+//
+//  SeasonTypeLabel.swift
+//  RookieKBO
+//
+//  Created by kyungsoolee on 10/18/24.
+//
+
+import SwiftUI
+
+struct SeasonTypeLabel: View {
+    
+    enum SeasonType: String {
+        case regular = "정규 시즌"
+        case post = "포스트 시즌"
+    }
+    
+    let seasonType: SeasonType
+
+    var body: some View {
+        Text(seasonType.rawValue)
+            .font(.Body.body2)
+            .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+            .foregroundStyle(TextLabel.tab)
+    }
+}
+
+#Preview {
+    SeasonTypeLabel(seasonType: SeasonTypeLabel.SeasonType.regular)
+}
