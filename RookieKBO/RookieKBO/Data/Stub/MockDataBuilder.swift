66070d6356b4b18ae8a2ae738a48c2a2b10aac7f
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-10-21 00:51:40 +0900

    [#24] 목데이터 수정

--- RookieKBO/RookieKBO/Data/Stub/MockDataBuilder.swift
+++ RookieKBO/RookieKBO/Data/Stub/MockDataBuilder.swift
@@ -171,248 +171,87 @@ struct MockDataBuilder {
         ]
     }
     
-    
     static var mockMatchList: [Match] {
+        let now = Date()
+        let calendar = Calendar.current
         return [
             Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: -5, to: Date())!,
+                startDateTime: calendar.date(bySettingHour: 18, minute: 30, second: 0, of: calendar.date(byAdding: .day, value: 0, to: now)!)!,
                 gameState: GameState.END,
                 homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
                 awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
                 place: "한화 생명 이글스 파크",
                 season: "포스트 시즌",
                 scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
+                    ScoreBoard(homeAndAway: .HOME, runs: 5, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
                     ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
                 ]
             ),
             Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: -4, to: Date())!,
+                startDateTime: calendar.date(bySettingHour: 14, minute: 0, second: 0, of: calendar.date(byAdding: .day, value: 0, to: now)!)!,
                 gameState: .PLAYING,
                 homeTeam: Team(name: "키움 히어로즈", image: "kiwoomUnder", color: "kiwoom", backgroundImage: "kiwoomBg"),
                 awayTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
                 place: "수원 위즈 파크",
                 season: "포스트 시즌",
                 scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 45, hits: 2, errors: 3, balls: 4, scores: [1, 2, 3, 4, 5, 6, 7, 8, 9]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 45, hits: 6, errors: 7, balls: 8, scores: [9, 8, 7, 6, 5, 4, 3, 2, 1])
+                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 2, errors: 3, balls: 4, scores: [1, 2, 0, 0]),
+                    ScoreBoard(homeAndAway: .AWAY, runs: 0, hits: 6, errors: 7, balls: 8, scores: [0, 0, 0, 0])
                 ]
             ),
             Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: -3, to: Date())!,
+                startDateTime: calendar.date(bySettingHour: 18, minute: 30, second: 0, of: calendar.date(byAdding: .day, value: 0, to: now)!)!,
                 gameState: GameState.END,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "한화 생명 이글스 파크",
-                season: "포스트 시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: -2, to: Date())!,
-                gameState: GameState.END,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "한화 생명 이글스 파크",
-                season: "포스트 시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: -1, to: Date())!,
-                gameState: GameState.END,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "한화 생명 이글스 파크",
-                season: "포스트 시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: -2, to: Date())!,
-                gameState: .PLAYING,
-                homeTeam: Team(name: "키움 히어로즈", image: "kiwoomUnder", color: "kiwoom", backgroundImage: "kiwoomBg"),
-                awayTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
-                place: "수원 위즈 파크",
-                season: "포스트 시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 45, hits: 2, errors: 3, balls: 4, scores: [1, 2, 3, 4, 5, 6, 7, 8, 9]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 45, hits: 6, errors: 7, balls: 8, scores: [9, 8, 7, 6, 5, 4, 3, 2, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .hour, value: 1, to: Date())!,
-                gameState: .PLAYING,
-                homeTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
-                awayTeam: Team(name: "키움 히어로즈", image: "kiwoomUnder", color: "kiwoom", backgroundImage: "kiwoomBg"),
-                place: "부산 사직야구장",
-                season: "정규시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 4, hits: 10, errors: 2, balls: 12, scores: [0, 2, 0, 1, 1]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 3, hits: 9, errors: 1, balls: 14, scores: [1, 0, 1, 0, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .hour, value: 0, to: Date())!,
-                gameState: .PLAYING,
-                homeTeam: Team(name: "SSG 랜더스", image: "ssgUnder", color: "ssg", backgroundImage: "ssgBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "인천 ssg랜더스필드",
-                season: "정규시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 4, hits: 10, errors: 2, balls: 12, scores: [0, 2, 0, 1, 1]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 3, hits: 9, errors: 1, balls: 14, scores: [1, 0, 1, 0, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .hour, value: 1, to: Date())!,
-                gameState: .PLAYING,
-                homeTeam: Team(name: "SSG 랜더스", image: "ssgUnder", color: "ssg", backgroundImage: "ssgBg"),
-                awayTeam: Team(name: "두산 베어스", image: "doosanUnder", color: "doosan", backgroundImage: "doosanBg"),
-                place: "인천 ssg랜더스필드",
-                season: "정규시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Date(),
-                gameState: GameState.END,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "한화 생명 이글스 파크",
-                season: "정규시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 3, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 2, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "대전 한화생명이글스파크",
-                season: "정규시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 2, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "대전 한화생명이글스파크",
-                season: "정규시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 0, to: Date())!,
-                gameState: .END,
-                homeTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
-                awayTeam: Team(name: "키움 히어로즈", image: "kiwoomUnder", color: "kiwoom", backgroundImage: "kiwoomBg"),
-                place: "부산 사직야구장",
-                season: "정규시즌",
-                scoreBoard: [
-                    ScoreBoard(homeAndAway: .HOME, runs: 4, hits: 10, errors: 2, balls: 12, scores: [0, 2, 0, 1, 1]),
-                    ScoreBoard(homeAndAway: .AWAY, runs: 3, hits: 9, errors: 1, balls: 14, scores: [1, 0, 1, 0, 1])
-                ]
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 0, to: Date())!,
-                gameState: .CANCEL,
-                homeTeam: Team(name: "삼성 라이온즈", image: "samsungUnder", color: "samsung", backgroundImage: "samsungBg"),
-                awayTeam: Team(name: "NC 다이노스", image: "ncUnder", color: "nc", backgroundImage: "ncBg"),
-                place: "대구 삼성라이온즈파크",
-                season: "정규시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 1, to: Date())!,
-                gameState: .CANCEL,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "한화 생명 이글스 파크",
-                season: "포스트 시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 1, to: Date())!,
-                gameState: .CANCEL,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "SSG 랜더스", image: "ssgUnder", color: "ssg", backgroundImage: "ssgBg"),
-                place: "한화 생명 이글스 파크",
-                season: "포스트 시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 2, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "KIA 타이거즈", image: "kiaUnder", color: "kia", backgroundImage: "kiaBg"),
-                awayTeam: Team(name: "SSG 랜더스", image: "ssgUnder", color: "ssg", backgroundImage: "ssgBg"),
-                place: "광주 기아챔피언스필드",
-                season: "정규시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 0, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "KT 위즈", image: "ktUnder", color: "kt", backgroundImage: "ktBg"),
-                awayTeam: Team(name: "NC 다이노스", image: "ncUnder", color: "nc", backgroundImage: "ncBg"),
-                place: "수원 위즈 파크",
-                season: "정규시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 0, to: Date())!,
-                gameState: .PREPARE,
                 homeTeam: Team(name: "KT 위즈", image: "ktUnder", color: "kt", backgroundImage: "ktBg"),
                 awayTeam: Team(name: "삼성 라이온즈", image: "samsungUnder", color: "samsung", backgroundImage: "samsungBg"),
-                place: "수원 위즈 파크",
+                place: "한화 생명 이글스 파크",
+                season: "포스트 시즌",
+                scoreBoard: [
+                    ScoreBoard(homeAndAway: .HOME, runs: 5, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1, 0 ]),
+                    ScoreBoard(homeAndAway: .AWAY, runs: 2, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0, 1, 1])
+                ]
+            ),
+            Match(
+                startDateTime: calendar.date(bySettingHour: 14, minute: 0, second: 0, of: calendar.date(byAdding: .day, value: 1, to: now)!)!,
+                gameState: GameState.PREPARE,
+                homeTeam: Team(name: "KIA 타이거즈", image: "kiaUnder", color: "kia", backgroundImage: "kiaBg"),
+                awayTeam: Team(name: "SSG 랜더스", image: "ssgUnder", color: "ssg", backgroundImage: "ssgBg"),
+                place: "한화 생명 이글스 파크",
+                season: "포스트 시즌",
+                scoreBoard: []
+            ),
+            Match(
+                startDateTime: calendar.date(bySettingHour: 18, minute: 30, second: 0, of: calendar.date(byAdding: .day, value: 0, to: now)!)!,
+                gameState: GameState.END,
+                homeTeam: Team(name: "KT 위즈", image: "ktUnder", color: "kt", backgroundImage: "ktBg"),
+                awayTeam: Team(name: "NC 다이노스", image: "ncUnder", color: "nc", backgroundImage: "ncBg"),
+                place: "한화 생명 이글스 파크",
+                season: "포스트 시즌",
+                scoreBoard: [
+                    ScoreBoard(homeAndAway: .HOME, runs: 5, hits: 8, errors: 1, balls: 15, scores: [1, 1, 1, 0, 1, 0, 1 ]),
+                    ScoreBoard(homeAndAway: .AWAY, runs: 0, hits: 7, errors: 0, balls: 5, scores: [0, 0, 0, 0, 0, 0, 0])
+                ]
+            ),
+            Match(
+                startDateTime: calendar.date(bySettingHour: 14, minute: 0, second: 0, of: calendar.date(byAdding: .day, value: 2, to: now)!)!,
+                gameState: .PREPARE,
+                homeTeam: Team(name: "두산 베어스", image: "doosanUnder", color: "doosan", backgroundImage: "doosanBg"),
+                awayTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
+                place: "서울 잠실 야구장",
+                season: "포스트 시즌",
+                scoreBoard: []
+            ),
+            Match(
+                startDateTime: calendar.date(bySettingHour: 18, minute: 30, second: 0, of: calendar.date(byAdding: .hour, value: 1, to: now)!)!,
+                gameState: .PLAYING,
+                homeTeam: Team(name: "롯데 자이언츠", image: "lotteUnder", color: "lotte", backgroundImage: "lotteBg"),
+                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
+                place: "부산 사직야구장",
                 season: "정규시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 0, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "대전 한화생명이글스파크",
-                season: "포스트 시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 1, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "대전 한화생명이글스파크",
-                season: "포스트 시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 2, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "대전 한화생명이글스파크",
-                season: "포스트 시즌",
-                scoreBoard: []
-            ),
-            Match(
-                startDateTime: Calendar.current.date(byAdding: .day, value: 3, to: Date())!,
-                gameState: .PREPARE,
-                homeTeam: Team(name: "한화 이글스", image: "hanwhaUnder", color: "hanwha", backgroundImage: "hanwhaBg"),
-                awayTeam: Team(name: "LG 트윈스", image: "lgUnder", color: "lg", backgroundImage: "lgBg"),
-                place: "대전 한화생명이글스파크",
-                season: "포스트 시즌",
-                scoreBoard: []
+                scoreBoard: [
+                    ScoreBoard(homeAndAway: .HOME, runs: 4, hits: 10, errors: 2, balls: 12, scores: [0, 2, 0, 1, 1]),
+                    ScoreBoard(homeAndAway: .AWAY, runs: 3, hits: 9, errors: 1, balls: 14, scores: [1, 0, 1, 0, 1])
+                ]
             )
         ]
     }
