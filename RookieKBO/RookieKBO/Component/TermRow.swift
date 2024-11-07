93a80ff7b8dfb40bf34e936119e2442f7bd39764
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-11-07 12:24:53 +0900

    [#43] 야구 용어 정리

---
+++ RookieKBO/RookieKBO/Component/TermRow.swift
@@ -0,0 +1,18 @@
+//
+//  TermRow.swift
+//  RookieKBO
+//
+//  Created by crownjoe on 11/7/24.
+//
+
+import SwiftUI
+
+struct TermRow: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    TermRow()
+}
