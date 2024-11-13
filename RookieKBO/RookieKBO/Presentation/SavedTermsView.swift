828245bd11ad4269e980291e3d73146a9c14c344
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-11-13 11:02:05 +0900

    [#55] 네비게이션 바 이슈 해결

--- RookieKBO/RookieKBO/Presentation/SavedTermsView.swift
+++ RookieKBO/RookieKBO/Presentation/SavedTermsView.swift
@@ -14,60 +14,55 @@ struct SavedTermsView: View {
     @State private var savedTermEntry: [TermEntry] = []
     @State private var isSaved: Bool = false
     
-    var backButton: some View {
-        Group {
-            Button {
-                pathModel.pop()
-            } label: {
-                Image(systemName: "chevron.left")
-                    .foregroundColor(.black)
-                    .frame(width: 36, height: 44)
-            }
-            
-            Spacer()
-            
-            Text("저장된 야구 용어")
-                .font(.Head.head5)
-                .foregroundColor(.gray7)
-            
-            Spacer()
-        }
-    }
-    
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(savedTermEntry, id: \.term) { termEntry in
-                    let isTermSaved = termUseCase.isTermSaved(term: termEntry.term)
-                    
-                    SavedTermView(
-                        isSaved: Binding(
-                            get: { isTermSaved },
-                            set: { newValue in
-                                if newValue {
-                                    termUseCase.createTermEntry(term: termEntry.term)
-                                } else {
-                                    termUseCase.deleteTermEntry(term: termEntry.term)
+        ZStack {
+            Color.gray2
+                .ignoresSafeArea(.all)
+            
+            ScrollView {
+                VStack {
+                    ForEach(savedTermEntry, id: \.term) { termEntry in
+                        let isTermSaved = termUseCase.isTermSaved(term: termEntry.term)
+                        
+                        SavedTermView(
+                            isSaved: Binding(
+                                get: { isTermSaved },
+                                set: { newValue in
+                                    if newValue {
+                                        termUseCase.createTermEntry(term: termEntry.term)
+                                    } else {
+                                        termUseCase.deleteTermEntry(term: termEntry.term)
+                                    }
+                                    self.savedTermEntry = termUseCase.state.savedTerms
+                                    termUseCase.printTermEntries()
                                 }
-                                self.savedTermEntry = termUseCase.state.savedTerms
-                                termUseCase.printTermEntries()
-                            }
-                        ),
-                        term: termEntry.term,
-                        description: termEntry.definition
-                    )
-                    .padding(.horizontal, 16)
-                    .padding(.bottom, 8)
+                            ),
+                            term: termEntry.term,
+                            description: termEntry.definition
+                        )
+                        .padding(.horizontal, 16)
+                        .padding(.bottom, 8)
+                    }
                 }
+                .padding(.top, 24)
             }
+            .onAppear {
+                self.savedTermEntry = termUseCase.state.savedTerms
+            }
+            
+            .navigationBarBackButtonHidden(true)
+            .navigationBarItems(leading: Button {
+                pathModel.pop()
+            } label : {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.gray7)
+            })
+            .navigationBarTitle("저장된 야구 용어", displayMode: .inline)
+            .background(Color.gray2)
         }
-        .onAppear {
-            self.savedTermEntry = termUseCase.state.savedTerms
-        }
-        .navigationBarBackButtonHidden(true)
-        .navigationBarItems(leading: backButton)
     }
 }
+
 
 #Preview {
     SavedTermsView()
