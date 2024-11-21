4d296e19397aa0a022857cded02495fc59f9ac33
omj0722 <omj0722@icloud.com>
2024-11-21 17:12:58 +0900

    [#90] 환경 설정

---
+++ RookieKBO/RookieKBO/Presentation/Highlight/HighlightTeamFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  HighlightTeamFilterView.swift
+//  RookieKBO
+//
+//  Created by Simmons on 11/21/24.
+//
+
+import SwiftUI
+
+struct HighlightTeamFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }}
+
+
+#Preview {
+    HighlightTeamFilterView()
+}
