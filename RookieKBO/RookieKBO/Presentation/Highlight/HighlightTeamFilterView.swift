9dd28c3811930903c5c3c87cf02573f10dfccb1c
omj0722 <omj0722@icloud.com>
2024-11-21 17:56:06 +0900

    [#90] 팀별 버튼 생성

--- RookieKBO/RookieKBO/Presentation/Highlight/HighlightTeamFilterView.swift
+++ RookieKBO/RookieKBO/Presentation/Highlight/HighlightTeamFilterView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct HighlightTeamFilterView: View {
+    
+    @Environment(SelectTeamUseCase.self) private var selectTeamUseCase
+    
+    @State private var selectedTeam: Team?
+    
+    let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }}
+        VStack {
+            LazyVGrid(columns: columns) {
+                ForEach(selectTeamUseCase.state.teams) { team in
+                    Button {
+                        selectedTeam = team
+                    } label: {
+                        HStack(spacing: 4) {
+                            Image(uiImage: convertUIImage(for: team.image)!)
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .offset(y: -4)
+                            Text(team.name.split(separator: " ")[0])
+                                .font(.Head.head4b)
+                                .foregroundColor(.white0)
+                                .frame(width: 88, height: 20)
+                        }
+                        .frame(width: 172, height: 48)
+                        .background(RoundedRectangle(cornerRadius: 99)
+                            .fill(Color.teamColor(for: team.color)!.opacity(selectedTeam == team ? 1 : 0.2))
+                        )
+                    }
+                    .padding(.bottom)
+                }
+            }
+            .padding(.horizontal)
+        }
+        .onAppear {
+            selectedTeam = selectTeamUseCase.state.selectedTeam
+        }
+    }
+}
 
 
 #Preview {
     HighlightTeamFilterView()
+        .environment(SelectTeamUseCase(selectTeamService: SelectTeamServiceImpl()))
 }
