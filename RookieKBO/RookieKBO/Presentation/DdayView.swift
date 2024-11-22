a00ce4777e309feb4320edc281db85cd117ae305
Seyeon CHO <135544903+crownjoe@users.noreply.github.com>
2024-11-23 01:50:08 +0900

    [#95] 네비게이션 연결

--- RookieKBO/RookieKBO/Presentation/DdayView.swift
+++ RookieKBO/RookieKBO/Presentation/DdayView.swift
@@ -15,63 +15,89 @@ struct DdayView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            Text("2025 KBO 리그")
-                .font(.Body.body1)
-                .foregroundColor(.white0)
-                .padding(.horizontal, 16)
-                .padding(.vertical, 10)
-                .background(Color.teamColor(for: currentTeam?.color ?? ""))
-                .cornerRadius(99)
-                .padding(.bottom, 20)
+            DdayInfo()
             
-            Text("개막까지 \(Date.daysLeftToOpeningDate() ?? 0)일 남았어요!")
-                .font(.Head.head4b)
-                .padding(.bottom, 12)
-                .foregroundColor(.gray7)
-                .padding(.leading, 16)
-            
-            Text("루키베이스도 준비중이에요! 함께 기다려요.")
-                .font(.Body.body2)
-                .padding(.bottom, 14)
-                .foregroundColor(.gray7)
-                .padding(.leading, 16)
-            
-            ZStack {
-                Image(.dayIcon)
-                    
-                if let teamImage = teamImage(for: currentTeam?.name ?? "") {
-                    Image(uiImage: teamImage)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 120, height: 120)
-                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 0)
-                } else {
-                    Image("allTeam")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 120, height: 120)
-                        .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 0)
-                }
+            VStack(alignment: .leading, spacing: 0) {
+                Text("2025 KBO 리그")
+                    .font(.Body.body1)
+                    .foregroundColor(.white0)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 10)
+                    .background(Color.teamColor(for: currentTeam?.color ?? ""))
+                    .cornerRadius(99)
+                    .padding(.bottom, 20)
                 
+                Text("개막까지 \(Date.daysLeftToOpeningDate() ?? 0)일 남았어요!")
+                    .font(.Head.head4b)
+                    .padding(.bottom, 12)
+                    .foregroundColor(.gray7)
+                    .padding(.leading, 16)
+                
+                Text("루키베이스도 준비중이에요! 함께 기다려요.")
+                    .font(.Body.body2)
+                    .padding(.bottom, 14)
+                    .foregroundColor(.gray7)
+                    .padding(.leading, 16)
+                
+                ZStack {
+                    Image(.dayIcon)
+                    
+                    if let teamImage = teamImage(for: currentTeam?.name ?? "") {
+                        Image(uiImage: teamImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 120, height: 120)
+                            .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 0)
+                    } else {
+                        Image("allTeam")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 120, height: 120)
+                            .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 0)
+                    }
+                    
+                }
+                .padding(.leading, 65)
+                .padding(.bottom, 12)
+                
+                DdayProgressView()
+                    .padding(.bottom, 24)
+                
+                Text("2025년 3월 22일 개막 예정")
+                    .font(.Body.body4)
+                    .padding(.horizontal, 12)
+                    .padding(.vertical, 8)
+                    .background(Color.gray3)
+                    .cornerRadius(10)
+                    .padding(.bottom, 24)
+                    .foregroundColor(.gray7)
+                    .padding(.leading, 73)
             }
-            .padding(.leading, 65)
-            .padding(.bottom, 12)
-            
-            DdayProgressView()
-                .padding(.bottom, 24)
-    
-            Text("2025년 3월 22일 개막 예정")
-                .font(.Body.body4)
-                .padding(.horizontal, 12)
-                .padding(.vertical, 8)
-                .background(Color.gray3)
-                .cornerRadius(10)
-                .padding(.bottom, 24)
-                .foregroundColor(.gray7)
-                .padding(.leading, 73)
+            .padding(.all, 12)
+            .background(.gray2)
+            .cornerRadius(18)
         }
+//        .padding(.horizontal, 16)
+        .padding(.vertical, 54)
+        .presentationDetents([.fraction(0.6)])
+        .presentationDragIndicator(.visible)
     }
 }
+
+// MARK: - DdayInfo
+
+private struct DdayInfo: View {
+    var body: some View {
+        Text("KBO 개막")
+            .font(.CustomTitle.customTitle2)
+            .padding(.bottom, 16)
+            .foregroundColor(.gray7)
+            .padding(.leading, 16)
+    }
+}
+
+
+// MARK: - DdayProgressView
 
 private struct DdayProgressView: View {
     let startDate = Date.postSeasonEnd
@@ -91,15 +117,13 @@ private struct DdayProgressView: View {
     }
     
     var body: some View {
-        VStack(spacing: 0) {
             ProgressBar(progress: progress)
-                .frame(width: 297, height: 16)
-                .cornerRadius(99)
-        }
     }
 }
 
-struct ProgressBar: View {
+// MARK: - ProgressBar
+
+private struct ProgressBar: View {
     @Environment(SelectTeamUseCase.self) private var selectTeamUseCase
     
     var currentTeam: Team? { selectTeamUseCase.state.selectedTeam }
@@ -109,15 +133,33 @@ struct ProgressBar: View {
     var body: some View {
         ZStack(alignment: .leading) {
             RoundedRectangle(cornerRadius: 99)
-                .fill(Color.teamColor(for: currentTeam?.color ?? "") ?? .brandPrimary).opacity(0.2)
+                .fill(Color.teamColor(for: currentTeam?.color ?? "") ?? .brandPrimary)
+                .opacity(0.2)
             
             RoundedRectangle(cornerRadius: 99)
                 .fill(Color.teamColor(for: currentTeam?.color ?? "") ?? .brandPrimary)
-                .frame(width: CGFloat(progress) * UIScreen.main.bounds.width)
-                .animation(.linear(duration: 0.1), value: progress)
+                .frame(width: CGFloat(progress) * 297)
+
+            ZStack {
+                Image(.speechBubble)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 30)
+                    .position(x: CGFloat(progress) * 297, y: -20)
+                    .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 0)
+                
+                Text("D-\(Date.daysLeftToOpeningDate() ?? 0)")
+                    .font(.Caption.caption1)
+                    .padding(.horizontal, 7)
+                    .padding(.vertical, 6)
+                    .foregroundColor(.gray7)
+                    .position(x: CGFloat(progress) * 297, y: -22)
+            }
         }
+        .frame(width: 297, height: 16)
     }
 }
+
 
 #Preview {
     DdayView()
