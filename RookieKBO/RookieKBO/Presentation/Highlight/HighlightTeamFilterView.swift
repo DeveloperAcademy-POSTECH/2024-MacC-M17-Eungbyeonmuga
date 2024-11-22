1548c827dde389104157de2819be4709022535ef
omj0722 <omj0722@icloud.com>
2024-11-22 23:37:09 +0900

    [#90] 팀 적용 버튼 구현

--- RookieKBO/RookieKBO/Presentation/Highlight/HighlightTeamFilterView.swift
+++ RookieKBO/RookieKBO/Presentation/Highlight/HighlightTeamFilterView.swift
@@ -11,6 +11,7 @@ struct HighlightTeamFilterView: View {
     
     @Environment(SelectTeamUseCase.self) private var selectTeamUseCase
     
+    @State private var filterColor: Color = .brandPrimary
     @State private var selectedTeam: Team?
     
     let columns = [GridItem(.flexible()), GridItem(.flexible())]
@@ -41,8 +42,34 @@ struct HighlightTeamFilterView: View {
                 }
             }
             .padding(.horizontal)
+            
+            Spacer()
+            
+            if selectedTeam?.name != selectTeamUseCase.state.selectedTeam?.name {
+                Button {
+                    // TODO: 해당 팀의 하이라이트만 표시
+                } label: {
+                    Text("이 팀의 하이라이트만 볼래요!")
+                        .font(.Head.head3)
+                        .frame(width: 361, height: 54)
+                        .foregroundColor(.white0)
+                        .background(
+                            RoundedRectangle(cornerRadius: 16)
+                                .fill(selectTeamUseCase.state.selectedTeam?.name == "전체 구단" ?
+                                      LinearGradient.gradient(
+                                        startColor: .brandPrimary,
+                                        endColor: .brandPrimaryGd
+                                      ) : LinearGradient.gradient(
+                                        startColor: filterColor,
+                                        endColor: filterColor
+                                      )
+                                     )
+                        )
+                }
+            }
         }
         .onAppear {
+            filterColor = Color.teamColor(for: selectTeamUseCase.state.selectedTeam?.color ?? "") ?? .brandPrimary
             selectedTeam = selectTeamUseCase.state.selectedTeam
         }
     }
