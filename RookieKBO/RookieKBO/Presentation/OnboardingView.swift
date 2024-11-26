8577007026c0f24b4abb4299e77781ec8b0af276
omj0722 <omj0722@icloud.com>
2024-11-26 11:27:17 +0900

    [#101] 하단 문구 변경

--- RookieKBO/RookieKBO/Presentation/OnboardingView.swift
+++ RookieKBO/RookieKBO/Presentation/OnboardingView.swift
@@ -13,8 +13,12 @@ struct OnboardingView: View {
     @State private var isAnimating: Bool = false
     @State private var currentImageIndex: Int = 0
     
+    let maxWidth = UIScreen.main.bounds.width
+    
     let bubbleScript = ["환영합니다!", "응원하는 팀을 선택해요", "우리 팀 기준의 경기 정보를 확인해요", "위젯으로 간편하게 확인해요", "하이라이트 영상과 함께 용어를 학습해요"]
     let onboardingImage = ["onboarding1", "onboarding2", "onboarding3", "onboarding4", "onboarding5"]
+    let titleScript = ["루키베이스", "우리 팀 설정", "경기 일정", "위젯", "하이라이트"]
+    let bodyScript = ["한국 야구 뉴비 팬들을 위한 야구 선배\n루키베이스를 소개합니다!", "내가 응원하는 팀을 선택하고\n우리 팀을 기준으로 정보를 확인해요.", "경기 일정과 상황, 종료 기록을\n날짜별로 쉽게 확인할 수 있어요.", "경기 정보 및 하이라이트 영상을 위젯으로 간편하게 확인해요.", "영상을 통해 야구 용어를 학습하거나\n키워드를 검색할 수 있어요."]
     
     var body: some View {
         ZStack {
@@ -31,7 +35,7 @@ struct OnboardingView: View {
                     HStack(spacing: 0) {
                         RoundedRectangle(cornerRadius: 16)
                             .fill(LinearGradient.gradient(startColor: .brandPrimary, endColor: .brandPrimaryGd))
-                            .frame(width: UIScreen.main.bounds.width * processGauge * 0.75, height: 16)
+                            .frame(width: maxWidth * processGauge * 0.75, height: 16)
                             .animation(.easeInOut(duration: 1.0), value: processGauge)
                             .padding(8)
                         
@@ -82,7 +86,7 @@ struct OnboardingView: View {
                                         Spacer(minLength: 0)
                                     }
                                 }
-                                .frame(width: UIScreen.main.bounds.width, height: 42)
+                                .frame(width: maxWidth, height: 42)
                                 .padding(.vertical, 24)
                                 
                                 Image(onboardingImage[index])
@@ -92,7 +96,7 @@ struct OnboardingView: View {
                             }
                         }
                     }
-                    .offset(x: CGFloat(currentImageIndex) * -(UIScreen.main.bounds.width))
+                    .offset(x: CGFloat(currentImageIndex) * -(maxWidth))
                     .animation(.easeInOut(duration: 0.8), value: currentImageIndex)
                 }
                 .disabled(true)
@@ -107,11 +111,11 @@ struct OnboardingView: View {
                 VStack(spacing: 0) {
                     HStack(spacing: 0) {
                         VStack(alignment: .leading, spacing: 15) {
-                            Text("루키베이스")
+                            Text(titleScript[Int(processGauge * 5) - 1])
                                 .font(.CustomTitle.customTitle2)
                                 .foregroundColor(.gray7)
                             
-                            Text("한국 야구 뉴비 팬들을 위한 야구 선배\n루키베이스를 소개합니다")
+                            Text(bodyScript[Int(processGauge * 5) - 1])
                                 .font(.Head.head4)
                                 .foregroundColor(.gray7)
                         }
@@ -123,16 +127,14 @@ struct OnboardingView: View {
                     ZStack {
                         HStack(spacing: 0) {
                             Button {
-                                if !isAnimating {
-                                    isAnimating = true
-                                    withAnimation(.easeInOut(duration: 0.2)) {
-                                        processGauge -= 0.2
-                                    }
-                                    currentImageIndex -= 1
-                                    
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                                        isAnimating = false
-                                    }
+                                isAnimating = true
+                                withAnimation(.easeInOut(duration: 0.2)) {
+                                    processGauge -= 0.2
+                                }
+                                currentImageIndex -= 1
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    isAnimating = false
                                 }
                             } label: {
                                 Image(systemName: "chevron.left")
@@ -157,7 +159,9 @@ struct OnboardingView: View {
                             Spacer(minLength: 0)
                             
                             Button {
-                                if !isAnimating {
+                                if processGauge == 1.0 {
+                                    // TODO: 메인 뷰로 이동
+                                } else {
                                     isAnimating = true
                                     withAnimation(.easeInOut(duration: 0.2)) {
                                         processGauge += 0.2
@@ -169,10 +173,10 @@ struct OnboardingView: View {
                                     }
                                 }
                             } label: {
-                                Text("다음")
+                                Text(processGauge == 1.0 ? "응원 팀 설정하러 갈래요!" : "다음")
                                     .font(.Head.head3)
                                     .foregroundColor(.white0)
-                                    .frame(width: processGauge > 0.21 ? 291 : 361, height: 54)
+                                    .frame(width: processGauge > 0.21 ? maxWidth - 102 : maxWidth - 32, height: 54)
                                     .background(RoundedRectangle(cornerRadius: 16)
                                         .fill(.brandPrimary)
                                     )
